0d794b0e0f0daf0d68ae088613501341666d3946
Pizdidal0609 <daniil.zaplyvanyi.ki.2023@lpnu.ua>
2024-06-11 23:27:19 +0300

    first task

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ new_list = []
 
 def func(list, new_list):
     for i in range(len(list)):
-        if list[i] == list[i]:
-            new_list.append(i)
-    return new_list
+        if list[i] == list[i-1]+1:
+            new_list.append(list[i])
+    print("New list is:", new_list) 
 
 def numcalc(new_list):
     result = 0
