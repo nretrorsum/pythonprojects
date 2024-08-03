b382823f3958475b9442c93f2cc45ad53a4da9e7
nretrorsum <daniil.zaplyvanyi.ki.2023@lpnu.ua>
2024-08-03 16:40:23 +0300

    First functional methods in app

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ data = [{"id": "1", "name":"kukumber", "balance": 120},
         {"id": "3", "name":"huesos", "balance": 654}]
 
 @app.get("/users/{user_id}")
-def get_user_id(user_id: str, ):
+def get_user_id(user_id: str):
     get_id = [user for user in data  if user.get("id") == user_id]
     return {"id":get_id}
 
